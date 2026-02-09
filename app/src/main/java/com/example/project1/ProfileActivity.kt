11f032abf4343c4f734f3979b5d8fa0b89555257
kati3smi <katismith@csumb.edu>
2026-02-08 18:54:48 -0800

    added buttons for changing username and password

--- app/src/main/java/com/example/project1/ProfileActivity.kt
+++ app/src/main/java/com/example/project1/ProfileActivity.kt
@@ -1,7 +1,7 @@
 package com.example.project1
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -20,87 +22,105 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.project1.ui.theme.Project1Theme
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_profile)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        @Composable
-        fun ProfileScreen() {
-            var username by remember { mutableStateOf("") }
-            var password by remember { mutableStateOf("") }
-            var newPassword by remember { mutableStateOf("")}
-            Column(
-                modifier = Modifier.fillMaxSize().padding(20.dp),
-                verticalArrangement = Arrangement.spacedBy(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                // Welcome text for specific user
-                Text(
-                    text = "Hi, ",
-                    style = MaterialTheme.typography.headlineMedium,
-                    fontWeight = FontWeight.Bold
-                )
-                // Text to indicate change username section
-                Text(
-                    text = "Change Username",
-                    style = MaterialTheme.typography.bodyLarge
-                )
-                // Text input for new username
-                OutlinedTextField(
-                    value = username,
-                    onValueChange = { username = it },
-                    label = { Text("New Username") },
-                    singleLine = true,
-                    modifier = Modifier.fillMaxWidth()
-                )
-                // Text input for password to change username
-                OutlinedTextField(
-                    value = password,
-                    onValueChange = { password = it },
-                    label = { Text("Password") },
-                    singleLine = true,
-                    modifier = Modifier.fillMaxWidth()
-                )
-                // Text to indicate change password section
-                Text(
-                    text = "Change Password",
-                    style = MaterialTheme.typography.bodyLarge
-                )
-                // Text input for old password to change password
-                OutlinedTextField(
-                    value = password,
-                    onValueChange = { password = it },
-                    label = { Text("Old Password") },
-                    singleLine = true,
-                    modifier = Modifier.fillMaxWidth()
-                )
-                // Text input for new password to change password
-                OutlinedTextField(
-                    value = newPassword,
-                    onValueChange = { password = it },
-                    label = { Text("New Password") },
-                    singleLine = true,
-                    modifier = Modifier.fillMaxWidth()
-                )
-                // Text input for new password to change password
-                OutlinedTextField(
-                    value = newPassword,
-                    onValueChange = { password = it },
-                    label = { Text("Re-enter New Password") },
-                    singleLine = true,
-                    modifier = Modifier.fillMaxWidth()
-                )
+        setContent {
+            Project1Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ProfileScreen(
+                        onChangeUsername = {},
+                        onChangePassword = {}
+                    )
+                }
             }
         }
+    }
+}
+
+@Composable
+private fun ProfileScreen(
+    onChangeUsername: () -> Unit,
+    onChangePassword: () -> Unit
+) {
+    var username by remember { mutableStateOf("") } // Input for username
+    var password by remember { mutableStateOf("") } // Input for password
+    var newPassword by remember { mutableStateOf("") } // Input for new password
+    Column(
+        modifier = Modifier.fillMaxSize().padding(20.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // Welcome text for specific user
+        Text(
+            text = "Hi, ",
+            style = MaterialTheme.typography.headlineMedium,
+            fontWeight = FontWeight.Bold
+        )
+        // Text to indicate change username section
+        Text(
+            text = "Change Username",
+            style = MaterialTheme.typography.bodyLarge
+        )
+        // Text input for new username
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text("New Username") },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth()
+        )
+        // Text input for password to change username
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth()
+        )
+        // Button to change username
+        OutlinedButton(
+            onClick = onChangeUsername,
+            modifier = Modifier.fillMaxWidth()
+        ) {Text("Change Username")}
+        // Text to indicate change password section
+        Text(
+            text = "Change Password",
+            style = MaterialTheme.typography.bodyLarge
+        )
+        // Text input for old password to change password
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Old Password") },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth()
+        )
+        // Text input for new password to change password
+        OutlinedTextField(
+            value = newPassword,
+            onValueChange = { password = it },
+            label = { Text("New Password") },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth()
+        )
+        // Text input to re-enter new password to change password
+        OutlinedTextField(
+            value = newPassword,
+            onValueChange = { password = it },
+            label = { Text("Re-enter New Password") },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth()
+        )
+        // Button to change username
+        OutlinedButton(
+            onClick = onChangePassword,
+            modifier = Modifier.fillMaxWidth()
+        ) {Text("Change Password")}
     }
 }
